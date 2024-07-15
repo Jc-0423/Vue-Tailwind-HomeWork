2d6c2bdfbda4e9310c2e1baae2512153c51aece1
Jc-0423 <jc0981606985@gmail.com>
2024-07-15 15:43:35 +0800

    fix: /修改無法顯示問題

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Vue-Tailwind-HomeWork',
+  base: 'Vue-Tailwind-HomeWork',
   plugins: [
     vue(),
   ],
